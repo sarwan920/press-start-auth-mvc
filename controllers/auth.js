42bbf58cd5a6ee4f39be98c9f560d69d4175fde1
ryanpilar <ryan_pilar@outlook.com>
2022-09-06 05:19:54 -0600

    index route update

--- controllers/auth.js
+++ controllers/auth.js
@@ -88,7 +88,7 @@ const User = require('../models/User')
     req.session.destroy((err) => {
       if (err) console.log('Error : Failed to destroy the session during logout.', err)
       req.user = null
-      res.redirect('/testIndex')
+      res.redirect('/')
     })
   }
   
